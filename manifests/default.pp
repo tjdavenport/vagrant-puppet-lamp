e9f7581f094474d461be93e2308c5780cc83df85
Torrance Davenport <torrance@fragmentlabs.com>
2015-04-12 16:44:46 -0400

    Adding zsh and git

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,6 +3,10 @@ import 'apache.pp'
 import 'php.pp'
 import 'mysql.pp'
 import 'nodejs.pp'
+import 'git.pp'
 
-class { "vim": }
+class { 'vim': }
+class { 'ohmyzsh': }
+ohmyzsh::install { 'vagrant': }
+
 
